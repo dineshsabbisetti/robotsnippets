76e0fe0fa2998db93bde1a1164158e9ea405d5c9
dineshsabbisetti <54508664+dineshsabbisetti@users.noreply.github.com>
2021-07-22 20:24:40 +0530

    Create longreceive.ino

---
+++ longreceive.ino
@@ -0,0 +1,112 @@
+// ########################## DEFINES ##########################
+#define HOVER_SERIAL_BAUD   115200      // [-] Baud rate for HoverSerial (used to communicate with the hoverboard)
+#define SERIAL_BAUD         115200      // [-] Baud rate for built-in Serial (used for the Serial Monitor)
+#define START_FRAME         0xABCD       // [-] Start frme definition for reliable serial communication
+// #define DEBUG_RX                        // [-] Debug received data. Prints all bytes to serial (comment-out to disable)
+
+//#include <SoftwareSerial.h>
+//SoftwareSerial HoverSerial(2,3);        // RX, TX
+
+// Global variables
+uint8_t idx = 0;                        // Index for new data pointer
+uint16_t bufStartFrame;                 // Buffer Start Frame
+byte *p;                                // Pointer declaration for the new received data
+byte incomingByte;
+byte incomingBytePrev;
+
+typedef struct{
+   signed long start;
+   signed long  cmd1;
+   signed long  cmd2;
+   signed long  speedR_meas;
+   signed long  speedL_meas;
+   signed long  batVoltage;
+   signed long  boardTemp;
+   signed long cmdLed;
+   signed long checksum;
+} SerialFeedback;
+SerialFeedback Feedback;
+SerialFeedback NewFeedback;
+
+// ########################## SETUP ##########################
+void setup() 
+{
+  Serial.begin(SERIAL_BAUD);
+  Serial.println("Hoverboard Serial v1.0");
+
+  Serial1.begin(HOVER_SERIAL_BAUD);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+// ########################## RECEIVE ##########################
+void Receive()
+{
+    // Check for new data availability in the Serial buffer
+    if (Serial1.available()) {
+        incomingByte    = Serial1.read();                                   // Read the incoming byte
+        bufStartFrame = ((uint16_t)(incomingByte) << 8) | incomingBytePrev;       // Construct the start frame
+    }
+    else {
+        return;
+    }
+
+  // If DEBUG_RX is defined print all incoming bytes
+  #ifdef DEBUG_RX
+        Serial.print(incomingByte);
+        return;
+    #endif
+
+    // Copy received data
+    if (bufStartFrame == START_FRAME) {                     // Initialize if new data is detected
+        p       = (byte *)&NewFeedback;
+        *p++    = incomingBytePrev;
+        *p++    = incomingByte;
+        idx     = 2;  
+    } else if (idx >= 2 && idx < sizeof(SerialFeedback)) {  // Save the new received data
+        *p++    = incomingByte; 
+        idx++;
+    } 
+    
+    // Check if we reached the end of the package
+    if (idx == sizeof(SerialFeedback)) {
+        signed long checksum;
+        checksum = (signed long)(NewFeedback.start ^ NewFeedback.cmd1 ^ NewFeedback.cmd2 ^ NewFeedback.speedR_meas ^ NewFeedback.speedL_meas
+                            ^ NewFeedback.batVoltage ^ NewFeedback.boardTemp ^ NewFeedback.cmdLed);
+
+        // Check validity of the new data
+        if (NewFeedback.start == START_FRAME && checksum == NewFeedback.checksum) {
+            // Copy the new data
+            memcpy(&Feedback, &NewFeedback, sizeof(SerialFeedback));
+
+            // Print data to built-in Serial
+            Serial.print("1: ");   Serial.print(Feedback.cmd1);
+            Serial.print(" 2: ");  Serial.print(Feedback.cmd2);
+            Serial.print(" 3: ");  Serial.print(Feedback.speedR_meas);
+            Serial.print(" 4: ");  Serial.print(Feedback.speedL_meas);
+            Serial.print(" 5: ");  Serial.print(Feedback.batVoltage);
+            Serial.print(" 6: ");  Serial.print(Feedback.boardTemp);
+            Serial.print(" 7: ");  Serial.println(Feedback.cmdLed);
+        } else {
+          Serial.println("Non-valid data skipped");
+        }
+        idx = 0;    // Reset the index (it prevents to enter in this if condition in the next cycle)
+    }
+
+    // Update previous states
+    incomingBytePrev = incomingByte;
+}
+
+// ########################## LOOP ##########################
+
+unsigned long iTimeSend = 0;
+
+void loop(void)
+{ 
+  unsigned long timeNow = millis();
+
+  // Check for new received data
+  Receive();
+  
+}
+
+// ########################## END ##########################
